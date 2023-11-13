2448e767df6c34e904ce1b6d3c29953b992d05ab
johan ROUYER <johanrouyer2@gmail.com>
2023-11-13 08:44:23 +0700

    add backend + screens

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,21 @@
-import data from './data';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <a href="/">FormulaRush</a>
-      </header>
-      <main>
-        <h1>Featured Products</h1>
-        <div className="products">
-          {data.products.map((product) => (
-            <div>
-              <div className="product" key={product.slug}>
-                <img src={product.image} alt={product.name} />
-                <p>{product.name}</p>
-                <p>{product.price}</p>
-              </div>
-            </div>
-          ))}
-        </div>
-      </main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">FormulaRush</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
