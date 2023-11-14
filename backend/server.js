76700e8210ec1f0772c800cd6b165f7af43ec228
johan ROUYER <johanrouyer2@gmail.com>
2023-11-14 13:13:49 +0700

    publish

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import productRouter from './routes/productRoutes.js';
 import express from 'express';
 import userRouter from './routes/userRoutes.js';
 import orderRouter from './routes/orderRoutes.js';
+import path from 'path';
 
 dotenv.config();
 
@@ -27,6 +28,12 @@ app.use('/api/orders', orderRouter);
 app.get('/api/keys/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
+
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
