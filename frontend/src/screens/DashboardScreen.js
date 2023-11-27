e3cf1ca4220ecacaa6c2d207fcdd36675ba6a816
johan ROUYER <johanrouyer2@gmail.com>
2023-11-27 20:40:49 +0700

    debug

--- frontend/src/screens/DashboardScreen.js
+++ frontend/src/screens/DashboardScreen.js
@@ -9,6 +9,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 
+// Définition du reducer pour gérer l'état de la requête
 const reducer = (state, action) => {
   switch (action.type) {
     case 'FETCH_REQUEST':
@@ -27,6 +28,7 @@ const reducer = (state, action) => {
 };
 
 const DashboardScreen = () => {
+  // Utilisation du reducer pour gérer l'état
   const [{ loading, summary, error }, dispatch] = useReducer(reducer, {
     loading: true,
     error: '',
@@ -34,35 +36,49 @@ const DashboardScreen = () => {
   const { state } = useContext(Store);
   const { userInfo } = state;
 
+  // Utilisation de useEffect pour effectuer la requête au chargement du composant
   useEffect(() => {
     const fetchData = async () => {
       try {
+        // Requête pour obtenir le résumé depuis le serveur
         const { data } = await axios.get('/api/orders/summary', {
           headers: { Authorization: `Bearer ${userInfo.token}` },
         });
+
+        // Mise à jour de l'état en cas de succès
         dispatch({ type: 'FETCH_SUCCESS', payload: data });
+
+        // Affichage des données dans la console (à des fins de débogage)
         console.log('Summary:', summary);
         console.log('Daily Orders:', summary.dailyOrders);
       } catch (err) {
+        // Mise à jour de l'état en cas d'échec avec affichage de l'erreur
         dispatch({
           type: 'FETCH_FAIL',
           payload: getError(err),
         });
       }
     };
+
+    // Appel de la fonction fetchData pour effectuer la requête
     fetchData();
   }, [userInfo]);
 
+  // Rendu du composant en fonction de l'état
   return (
     <div>
       <h1>Dashboard</h1>
       {loading ? (
+        // Affichage du composant de chargement en cas de requête en cours
         <LoadingBox />
       ) : error ? (
+        // Affichage du composant MessageBox en cas d'erreur
         <MessageBox variant="danger">{error}</MessageBox>
       ) : (
+        // Affichage des données du résumé si la requête est réussie
         <>
           <Row>
+            {/* Colonne pour les cartes utilisateur, commandes et ventes totales */}
             <Col md={4}>
               <Card>
                 <Card.Body>
@@ -97,11 +113,14 @@ const DashboardScreen = () => {
               </Card>
             </Col>
           </Row>
+          {/* Section des ventes */}
           <div className="my-3">
             <h2>Sales</h2>
             {summary.dailyOrders && summary.dailyOrders.length === 0 ? (
+              // Affichage d'un message si aucune vente n'est disponible
               <MessageBox>No Sale</MessageBox>
             ) : (
+              // Affichage du graphique des ventes avec React Google Charts
               <Chart
                 width="100%"
                 height="400px"
@@ -114,11 +133,14 @@ const DashboardScreen = () => {
               ></Chart>
             )}
           </div>
+          {/* Section des catégories */}
           <div className="my-3">
             <h2>Categories</h2>
             {summary.productCategories.length === 0 ? (
+              // Affichage d'un message si aucune catégorie n'est disponible
               <MessageBox>No Category</MessageBox>
             ) : (
+              // Affichage du graphique des catégories avec React Google Charts
               <Chart
                 width="100%"
                 height="400px"
